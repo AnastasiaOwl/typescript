9d81bb0a408966dab41fb4c4c4193d157154b0b8
Filina Anastasia <124703588+AnastasiaOwl@users.noreply.github.com>
2024-12-16 10:41:35 +0200

    change any to unknown

--- index.ts
+++ index.ts
@@ -223,6 +223,6 @@
 
 type Arr = ['1', '2', '3']
 
-type TupleToUnion<T extends any[]>= T[number];
+type TupleToUnion<T extends unknown[]>= T[number];
 
 type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
